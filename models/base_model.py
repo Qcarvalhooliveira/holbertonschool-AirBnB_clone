74e9a37369b63c372498dec383731becfdd4d06b
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-04 16:17:35 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,11 @@ class BaseModel:
 
     def __str__(self):
         """Returns string representation of the BaseModel class"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute updated_at with the 
+        """Updates the public instance attribute updated_at with the
            current datetime"""
         self.updated_at = datetime.now()
 
@@ -29,4 +30,3 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
-
