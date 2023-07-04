2eb1fe4aea2f72364e03c1a590840e35f61e2f81
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-04 17:05:53 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -29,4 +29,5 @@ class BaseModel:
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
+
         return new_dict
