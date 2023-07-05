061adc2922712ff9060d91e3b32e18216ed3eef4
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-05 12:41:44 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,20 @@ class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Class constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        models.storage.new(self)
 
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value,
-                                                  '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """Returns string representation of the BaseModel class"""
