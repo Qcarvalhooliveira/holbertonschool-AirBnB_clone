6dd77f0b708c28e2bc80c2b186f1573e4063ca3b
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-05 22:49:59 +0200

    test_review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Unittest for Review class
+"""
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    """
+    Test cases for Review class
+    """
+    def test_inheritance(self):
+        """
+        Test if Review class inherits from BaseModel
+        """
+        review = Review()
+        self.assertIsInstance(review, BaseModel)
+
+    def test_attributes(self):
+        """
+        Test if the attributes are correctly initialized
+        """
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+    def test_initialization(self):
+        """
+        Test if the attributes are correctly initialized with specific values
+        """
+        review = Review(place_id="123", user_id="456", text="Good job!")
+        self.assertEqual(review.place_id, "123")
+        self.assertEqual(review.user_id, "456")
+        self.assertEqual(review.text, "Good job!")
+
+
+if __name__ == '__main__':
+    unittest.main()
