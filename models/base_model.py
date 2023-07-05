c8f720151423c72062023d8dfa99f7486c6ee696
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-07-05 14:45:58 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,27 @@
 
 import uuid
 from datetime import datetime
+import models
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Class constructor"""
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """Returns string representation of the BaseModel class"""
@@ -23,6 +34,7 @@ class BaseModel:
         """Updates the public instance attribute updated_at with the
            current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
