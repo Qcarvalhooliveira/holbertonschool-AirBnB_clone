4b8cd5bd177b1083bfd335349da19075b8a007ca
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-05 12:48:46 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -2,29 +2,17 @@
 """Module for class BaseModel"""
 
 import uuid
-import models
 from datetime import datetime
-from file_storage import storage
 
 
 class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Class constructor"""
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns string representation of the BaseModel class"""
@@ -35,7 +23,6 @@ class BaseModel:
         """Updates the public instance attribute updated_at with the
            current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
