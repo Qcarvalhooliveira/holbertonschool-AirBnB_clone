6891e384a7e13feaaed1c8d541816e3cde56acac
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-05 15:15:08 +0200

    __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module that initialize the models package"""
 
-from base_model import BaseModel
-from file_storage import FileStorage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
