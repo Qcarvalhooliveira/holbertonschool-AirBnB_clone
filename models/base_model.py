1d4a06f17116d52366af827df9aca780815d9c06
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-05 12:19:04 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -31,6 +31,7 @@ class BaseModel:
         """Updates the public instance attribute updated_at with the
            current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
