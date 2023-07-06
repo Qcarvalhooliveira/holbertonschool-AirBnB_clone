3b038eecf5a0f8793e922c90c3dbde4a52a7dc93
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-06 19:57:14 +0200

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,31 +22,29 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
         new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as file:
-            json.dump(new_dict, file)
+        for key, value in self.__objects():
+            new_json[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, mode='w', encoding='utf-8') as myFile:
+            json.dump(new_json, myfile)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, mode='r',
-                      encoding='utf-8') as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
-                    class_name, obj_id = key.split('.')
-                    cls = globals()[class_name]
-                    obj = cls(**value)
-                    FileStorage.__objects[key] = obj
+            with open(self.__file_path, mode='r',
+                      encoding='utf-8') as myFile:
+                json_obj = json.load(myFile)
+                for key, value in json_obj.items():
+                    name = classes[value["__class__"]](**value)
+                    self.__objects[key] = name
         except FileNotFoundError:
             pass
