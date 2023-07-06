935f09aec009e54a558c873c16e23af29eca7cae
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-06 19:11:16 +0200

    console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 Module for the command interpreter
 """
 import cmd
+from datetime import datetime
+import shlex
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
@@ -47,81 +49,114 @@ class HBNBCommand(cmd.Cmd):
         Creates a new instance of BaseModel, saves it (to the JSON file),
         and prints the id.
         """
-        if not arg:
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-        elif arg not in classes:
-            print("** class doesn't exist **")
+            return False
+        if args[0] in classes:
+            instance = classes[args[0]]()
         else:
-            new_instance = classes[arg]()
-            new_instance.save()
-            print(new_instance.id)
+            print("** class doesn't exist **")
+            return False
+        print(new_instance.id)
+        instance.save()
 
     def do_show(self, arg):
         """
         Prints the string representation of an instance based on the
         class name and id.
         """
-        args = arg.split()
-        if not args:
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        else:
-            obj_dict = storage.all()
-            key = args[0] + "." + args[1]
-            if key in obj_dict:
-                print(obj_dict[key])
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in storage.all():
+                    print(storage.all()[key])
+                else:
+                    print("** no instance found **")
             else:
-                print("** no instance found **")
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_destroy(self, arg):
         """
-        Deletes an instance based on the class name and id.
+        Deletes an instance based on the class name and id - save the
+        change into the JSON file
         """
-        args = arg.split()
-        if not args:
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        else:
-            obj_dict = storage.all()
-            key = args[0] + "." + args[1]
-            if key in obj_dict:
-                del obj_dict[key]
-                storage.save()
+        elif args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in storage.all():
+                    storage.all().pop(key)
+                    storage.save()
+                else:
+                    print("** no instance found **")
             else:
-                print("** no instance found **")
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_all(self, arg):
         """
-        Prints all string representations of all instances based or not
-        on the class name.
+        Prints all string representation of all instances based or not on the
+        class name
         """
-        args = arg.split()
-        obj_dict = storage.all()
-        if not args:
-            print([str(obj_dict[key]) for key in obj_dict])
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
+        args = shlex.split(arg)
+        all_objects = []
+        if len(args) == 0:
+            for value in storage.all().values():
+                all_objects.append(str(value))
+            print("[", end="")
+            print(", ".join(all_objects), end="")
+            print("]")
+        elif args[0] in classes:
+            for key in storage.all():
+                if args[0] in key:
+                    all_objects.append(str(storage.all()[key]))
+            print("[", end="")
+            print(", ".join(all_objects), end="")
+            print("]")
         else:
-            print([str(obj_dict[key]) for key in obj_dict if
-                  key.startswith(args[0])])
+            print("** class doesn't exist **")
 
     def do_update(self, arg):
         """
         Updates an instance based on the class name and id by adding or
-        updating an attribute.
+        updating attribute (save the change into the JSON file)
         """
-        args = arg.split()
-        if not args:
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in classes:
+        elif args[0] in classes:
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            key = args[0] + "." + args[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                obj = storage.all()[key]
+                immutable_attrs = ["id", "created_at", "updated_at"]
+                if obj:
+                    tokens = shlex.split(arg)
+                    if len(tokens) < 3:
+                        print("** attribute name missing **")
+                    elif len(tokens) < 4:
+                        print("** value missing **")
+                    elif tokens[2] not in immutable_attrs:
+                        obj.__dict__[tokens[2]] = tokens[3]
+                        obj.updated_at = datetime.now()
+                        storage.save()
+        else:
             print("** class doesn't exist **")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
