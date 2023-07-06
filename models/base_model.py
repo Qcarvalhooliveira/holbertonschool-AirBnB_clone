cd74f9ff86aacc5bd88a82b72b48df66173dc6f0
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-06 20:11:00 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
+        new_dict['__class__'] = str(type(self).__name__)
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
 
