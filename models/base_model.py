fc78afa8e85ad3b54ee35c8dd5c298baa25c3aec
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-06 20:18:44 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,13 @@ time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """Class that defines all common attributes/methods for other classes"""
+    """
+    Class that defines all common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
-        """Class constructor"""
+        """
+        Class constructor
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -27,18 +31,24 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """Returns string representation of the BaseModel class"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """
+        Returns string representation of the BaseModel class
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__,
+                                         self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute updated_at with the
-           current datetime"""
+        """
+        Updates the public instance attribute updated_at with the
+        current datetime
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__"""
+        """
+        Returns a dictionary containing all keys/values of __dict__
+        """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = str(type(self).__name__)
         new_dict['created_at'] = self.created_at.isoformat()
