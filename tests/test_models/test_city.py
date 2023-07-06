15a3d51acde2fc8527e9d86aeefe7470373a0961
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-06 11:21:24 +0200

    test_city

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Unittest for city class
+"""
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+    def test_inheritance(self):
+        """
+        Test if City class inherits from BaseModel
+        """
+        city = City()
+        self.assertIsInstance(city, BaseModel)
+
+    def test_attributes(self):
+        """
+        Test if the attributes are correctly initialized
+        """
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
+
+    def test_initialization(self):
+        """
+        Test if the attributes are correctly initialized with specific values
+        """
+        city = City(state_id="123", name="San Francisco")
+        self.assertEqual(city.state_id, "123")
+        self.assertEqual(city.name, "San Francisco")
+
+
+if __name__ == '__main__':
+    unittest.main()
