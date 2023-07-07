cbb676f0f808cc0c21524096daef1d6348baba88
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-07-07 14:46:53 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -8,6 +8,3 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Class representing an amenity"""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
