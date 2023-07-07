3e4c476b25d0fda5472fc74afbe1c236cbbb9fe9
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-07 11:56:56 +0200

    test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,9 +10,14 @@ from models.base_model import BaseModel
 from models.user import User
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
+           "State": State, "City": City, "Amenity": Amenity, "Review": Review}
 
 
 class TestFileStorage(unittest.TestCase):
